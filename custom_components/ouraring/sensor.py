6087bf645a9e8a6f7ee0685137968dd3feb058cc
Adam Keslo <adam.keslowitz@gmail.com>
2022-10-22 22:40:15 -0400

    Added Readiness & Activity/Workouts

--- custom_components/ouraring/sensor.py
+++ custom_components/ouraring/sensor.py
@@ -21,6 +21,10 @@ OURA_SCHEMA = vol.Schema(
 )
 
 
+def tdelta_to_hour_min(td):
+    return str(td.seconds // 3600) + "hr " + str((td.seconds // 60) % 60) + "min"
+
+
 def _seconds_to_hours(time_in_seconds):
     """Parses times in seconds and converts it to hours."""
     return round(int(time_in_seconds) / (60 * 60), 2)
@@ -37,7 +41,13 @@ def setup_platform(
     discovery_info: DiscoveryInfoType | None = None,
 ) -> None:
     """Set up the sensor platform."""
-    add_entities([OuraSleep(config, hass)])
+    add_entities(
+        [
+            OuraSleep(config, hass),
+            OuraReadiness(config, hass),
+            OuraActivity(config, hass),
+        ]
+    )
 
 
 class OuraSleep(Entity):
@@ -94,7 +104,7 @@ class OuraSleep(Entity):
 
         if "data" in daily_sleep_response:
             self._state = daily_sleep_response["data"][0]["score"]
-            logging.info("OuraRing: Score Updated: %s", self._state)
+            logging.info("OuraRing: Sleep Score Updated: %s", self._state)
 
             sleep_response = api.get_data(self._oura_token, oura_api.OuraURLs.SLEEP)[
                 "data"
@@ -102,7 +112,6 @@ class OuraSleep(Entity):
 
             for item in sleep_response:
                 if item["type"] == "long_sleep":
-                    logging.info("OuraRing: Updating Sleep Data: %s", item)
                     bedtime_start = parser.parse(item["bedtime_start"])
                     bedtime_end = parser.parse(item["bedtime_end"])
 
@@ -128,4 +137,158 @@ class OuraSleep(Entity):
                         ),
                         "awake_duration": _seconds_to_hours(item["awake_time"]),
                         "in_bed_duration": _seconds_to_hours(item["time_in_bed"]),
+                        "day": item["day"],
                     }
+                    logging.info("OuraRing: Updated Sleep Data: %s", self._attributes)
+
+
+class OuraReadiness(Entity):
+
+    """Representation of a Sensor."""
+
+    def __init__(self, config, hass):
+        """Initialize the sensor."""
+        self._state = 0
+        self._attributes = {}
+        self._oura_token = config.get(CONF_API_TOKEN)
+
+    @property
+    def name(self) -> str:
+        """Return the name of the sensor."""
+        return "Oura Ring Readiness"
+
+    @property
+    def state(self) -> int:
+        """Return the state of the sensor."""
+        return self._state
+
+    @property
+    def icon(self) -> str:
+        """Return the state of the sensor."""
+        return "mdi:account-alert"
+
+    @property
+    def unit_of_measurement(self) -> str:
+        """Return the unit of measurement."""
+        return ""
+
+    @property
+    # pylint: disable=hass-return-type
+    def extra_state_attributes(self):
+        return self._attributes
+
+    def update(self) -> None:
+        """Fetch new state data for the sensor.
+
+        This is the only method that should fetch new data for Home Assistant.
+        """
+        api = oura_api.OuraAPI()
+
+        daily_readiness_response = api.get_data(
+            self._oura_token, oura_api.OuraURLs.DAILY_READINESS
+        )
+
+        if "data" in daily_readiness_response:
+            self._state = daily_readiness_response["data"][0]["score"]
+            logging.info("OuraRing: Readiness Score Updated: %s", self._state)
+
+
+class OuraActivity(Entity):
+
+    """Representation of a Sensor."""
+
+    def __init__(self, config, hass):
+        """Initialize the sensor."""
+        self._state = 0
+        self._attributes = {}
+        self._oura_token = config.get(CONF_API_TOKEN)
+
+    @property
+    def name(self) -> str:
+        """Return the name of the sensor."""
+        return "Oura Ring Activity"
+
+    @property
+    def state(self) -> int:
+        """Return the state of the sensor."""
+        return self._state
+
+    @property
+    def icon(self) -> str:
+        """Return the state of the sensor."""
+        return "mdi:run-fast"
+
+    @property
+    def unit_of_measurement(self) -> str:
+        """Return the unit of measurement."""
+        return ""
+
+    @property
+    # pylint: disable=hass-return-type
+    def extra_state_attributes(self):
+        return self._attributes
+
+    def update(self) -> None:
+        """Fetch new state data for the sensor.
+
+        This is the only method that should fetch new data for Home Assistant.
+        """
+        api = oura_api.OuraAPI()
+
+        daily_activity_response = api.get_data(
+            self._oura_token, oura_api.OuraURLs.DAILY_ACTIVITY
+        )
+
+        if "data" in daily_activity_response:
+            self._state = daily_activity_response["data"][0]["score"]
+            logging.info("OuraRing: Activity Score Updated: %s", self._state)
+
+            self._attributes["data"] = {
+                "active_calories": daily_activity_response["data"][0][
+                    "active_calories"
+                ],
+                "total_calories": daily_activity_response["data"][0]["total_calories"],
+                "inactivity_alerts": daily_activity_response["data"][0][
+                    "inactivity_alerts"
+                ],
+                "non_wear_time": daily_activity_response["data"][0]["non_wear_time"],
+                "steps": daily_activity_response["data"][0]["steps"],
+                "target_calories": daily_activity_response["data"][0][
+                    "target_calories"
+                ],
+            }
+            logging.info("OuraRing: Updated Activity Data: %s", self._attributes)
+
+            # Get Workout Data
+            daily_workout_response = api.get_data(
+                self._oura_token, oura_api.OuraURLs.WORKOUT
+            )
+            if "data" in daily_workout_response:
+                workouts = daily_workout_response["data"]
+                i = 1
+                for item in workouts:
+                    end_datetime = parser.parse(item["end_datetime"]).strftime("%H:%M")
+                    start_datetime = parser.parse(item["start_datetime"]).strftime(
+                        "%H:%M"
+                    )
+                    FMT = "%H:%M"
+                    tdelta = datetime.strptime(end_datetime, FMT) - datetime.strptime(
+                        start_datetime, FMT
+                    )
+                    self._attributes["workout_" + str(i)] = {
+                        "activity": item["activity"],
+                        "calories": item["calories"],
+                        "distance": item["distance"],
+                        "start_datetime": start_datetime,
+                        "end_datetime": end_datetime,
+                        "duration": tdelta_to_hour_min(tdelta),
+                        "intensity": item["intensity"],
+                        "label": item["label"],
+                        "source": item["source"],
+                        "day": item["day"],
+                    }
+                    logging.info(
+                        "OuraRing: Updated Workout Data: %s",
+                        self._attributes["workout_" + str(i)],
+                    )
+                    i = i + 1
