2c6e2c895ef9519c1c5f4a5e52799da6fc45c501
Adam Keslo <akeslo@users.noreply.github.com>
2022-10-29 16:09:12 -0400

    Update to only update sensor if the data pulled is today
    I have seen instances from the API where data from today does not arrive.  Previously yesterday's score was populating.

--- custom_components/ouraring/sensor.py
+++ custom_components/ouraring/sensor.py
@@ -100,46 +100,52 @@ class OuraSleep(Entity):
         api = oura_api.OuraAPI()
         yest_string = _get_date_string(-1)
         tom_string = _get_date_string(1)
+        tod_string = _get_date_string(0)
         daily_sleep_response = api.get_data(
             self._oura_token, oura_api.OuraURLs.DAILY_SLEEP, yest_string, tom_string
         )
         if "data" in daily_sleep_response:
-            self._state = daily_sleep_response["data"][(len(daily_sleep_response["data"]) - 1)]["score"]
-            logging.info("OuraRing: Sleep Score Updated: %s", self._state)
-            sleep_response = api.get_data(
-                self._oura_token, oura_api.OuraURLs.SLEEP, yest_string, tom_string
-            )["data"]
+            sleep = daily_sleep_response["data"][(len(daily_sleep_response["data"]) - 1)]
+            day = daily_sleep_response["data"][(len(daily_sleep_response["data"]) - 1)]["day"]
+            score = daily_sleep_response["data"][(len(daily_sleep_response["data"]) - 1)]["score"]
+            if day == tod_string:
 
-            for item in sleep_response:
-                if item["type"] == "long_sleep":
-                    bedtime_start = parser.parse(item["bedtime_start"])
-                    bedtime_end = parser.parse(item["bedtime_end"])
+                self._state = score
+                logging.info("OuraRing: Sleep Score Data: %s", sleep)
+                logging.info("OuraRing: Sleep Score Updated: %s", self._state)
+                sleep_response = api.get_data(
+                    self._oura_token, oura_api.OuraURLs.SLEEP, yest_string, tom_string
+                )["data"]
 
-                    self._attributes["data"] = {
-                        "date": item["day"],
-                        "bedtime_start_hour": bedtime_start.strftime("%H:%M"),
-                        "bedtime_end_hour": bedtime_end.strftime("%H:%M"),
-                        "breath_average": item["average_breath"],
-                        "temperature_delta": item["readiness"]["temperature_deviation"],
-                        "lowest_heart_rate": item["lowest_heart_rate"],
-                        "heart_rate_average": item["average_heart_rate"],
-                        "deep_sleep_duration": _seconds_to_hours(
-                            item["deep_sleep_duration"]
-                        ),
-                        "rem_sleep_duration": _seconds_to_hours(
-                            item["rem_sleep_duration"]
-                        ),
-                        "light_sleep_duration": _seconds_to_hours(
-                            item["light_sleep_duration"]
-                        ),
-                        "total_sleep_duration": _seconds_to_hours(
-                            item["total_sleep_duration"]
-                        ),
-                        "awake_duration": _seconds_to_hours(item["awake_time"]),
-                        "in_bed_duration": _seconds_to_hours(item["time_in_bed"]),
-                    }
-                    logging.info("OuraRing: Updated Sleep Data: %s", self._attributes)
+                for item in sleep_response:
+                    if item["type"] == "long_sleep":
+                        bedtime_start = parser.parse(item["bedtime_start"])
+                        bedtime_end = parser.parse(item["bedtime_end"])
 
+                        self._attributes["data"] = {
+                            "date": item["day"],
+                            "bedtime_start_hour": bedtime_start.strftime("%H:%M"),
+                            "bedtime_end_hour": bedtime_end.strftime("%H:%M"),
+                            "breath_average": item["average_breath"],
+                            "temperature_delta": item["readiness"]["temperature_deviation"],
+                            "lowest_heart_rate": item["lowest_heart_rate"],
+                            "heart_rate_average": item["average_heart_rate"],
+                            "deep_sleep_duration": _seconds_to_hours(
+                                item["deep_sleep_duration"]
+                            ),
+                            "rem_sleep_duration": _seconds_to_hours(
+                                item["rem_sleep_duration"]
+                            ),
+                            "light_sleep_duration": _seconds_to_hours(
+                                item["light_sleep_duration"]
+                            ),
+                            "total_sleep_duration": _seconds_to_hours(
+                                item["total_sleep_duration"]
+                            ),
+                            "awake_duration": _seconds_to_hours(item["awake_time"]),
+                            "in_bed_duration": _seconds_to_hours(item["time_in_bed"]),
+                        }
+                        logging.info("OuraRing: Updated Sleep Data: %s", self._attributes)
 
 # ACTIVITY
 class OuraReadiness(Entity):
@@ -185,15 +191,18 @@ class OuraReadiness(Entity):
         api = oura_api.OuraAPI()
         yest_string = _get_date_string(-1)
         tom_string = _get_date_string(1)
+        tod_string = _get_date_string(0)
         daily_readiness_response = api.get_data(
             self._oura_token, oura_api.OuraURLs.DAILY_READINESS, yest_string, tom_string
         )
 
         if "data" in daily_readiness_response:
             index = len(daily_readiness_response["data"]) - 1
-            self._state = daily_readiness_response["data"][index]["score"]
-            logging.info("OuraRing: Readiness Score Updated: %s", self._state)
-
+            score = daily_readiness_response["data"][index]["score"]
+            day = daily_readiness_response["data"][index]["day"]
+            if day == tod_string:
+                self._state = score
+                logging.info("OuraRing: Readiness Score Updated: %s", self._state)
 
 # READINESS
 class OuraActivity(Entity):
@@ -237,41 +246,46 @@ class OuraActivity(Entity):
         This is the only method that should fetch new data for Home Assistant.
         """
         api = oura_api.OuraAPI()
+        one_week_ago = _get_date_string(-8)
         yest_string = _get_date_string(-1)
         tom_string = _get_date_string(1)
+        tod_string = _get_date_string(0)
         daily_activity_response = api.get_data(
             self._oura_token, oura_api.OuraURLs.DAILY_ACTIVITY, yest_string, tom_string
         )
 
         if "data" in daily_activity_response:
             index = len(daily_activity_response["data"]) - 1
-            self._state = daily_activity_response["data"][index]["score"]
-            logging.info("OuraRing: Activity Score Updated: %s", self._state)
+            day =  daily_activity_response["data"][index]["day"]
+            if day == tod_string:
+                score =  daily_activity_response["data"][index]["score"]
+                self._state = score
+                logging.info("OuraRing: Activity Score Updated: %s", self._state)
 
-            self._attributes["data"] = {
-                "active_calories": daily_activity_response["data"][index][
-                    "active_calories"
-                ],
-                "total_calories": daily_activity_response["data"][index][
-                    "total_calories"
-                ],
-                "inactivity_alerts": daily_activity_response["data"][index][
-                    "inactivity_alerts"
-                ],
-                "non_wear_time": daily_activity_response["data"][index][
-                    "non_wear_time"
-                ],
-                "steps": daily_activity_response["data"][index]["steps"],
-                "target_calories": daily_activity_response["data"][index][
-                    "target_calories"
-                ],
-                "date": daily_activity_response["data"][index]["day"],
-            }
-            logging.info("OuraRing: Updated Activity Data: %s", self._attributes)
+                self._attributes["data"] = {
+                    "active_calories": daily_activity_response["data"][index][
+                        "active_calories"
+                    ],
+                    "total_calories": daily_activity_response["data"][index][
+                        "total_calories"
+                    ],
+                    "inactivity_alerts": daily_activity_response["data"][index][
+                        "inactivity_alerts"
+                    ],
+                    "non_wear_time": daily_activity_response["data"][index][
+                        "non_wear_time"
+                    ],
+                    "steps": daily_activity_response["data"][index]["steps"],
+                    "target_calories": daily_activity_response["data"][index][
+                        "target_calories"
+                    ],
+                    "date": daily_activity_response["data"][index]["day"],
+                }
+                logging.info("OuraRing: Updated Activity Data: %s", self._attributes)
 
             # Get Workout Data
             daily_workout_response = api.get_data(
-                self._oura_token, oura_api.OuraURLs.WORKOUT, yest_string, tom_string
+                self._oura_token, oura_api.OuraURLs.WORKOUT, one_week_ago, tom_string
             )
             if "data" in daily_workout_response:
                 workouts = daily_workout_response["data"]
